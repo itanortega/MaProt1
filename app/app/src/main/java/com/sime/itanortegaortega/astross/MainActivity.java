af47e7693efd049733b204d35bccc7039070aa67
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-04-17 22:52:52 -0500

    Agregada segunda actividad, signo individual

--- app/app/src/main/java/com/sime/itanortegaortega/astross/MainActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sime.itanortegaortega.astross;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,17 +9,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView signo = null;
-    TextView fechas = null;
-    ImageView img_view = null;
+    TextView Txt_Nombre_Signo = null;
+    TextView Txt_Fechas = null;
+    ImageView ImgVCentral = null;
+    int id=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signo = (TextView) this.findViewById(R.id.Txt_Nombre_Signo);
-        fechas = (TextView) this.findViewById(R.id.Txt_Fechas);
-        img_view = (ImageView) this.findViewById(R.id.ImgVCentral);
+        Txt_Nombre_Signo = (TextView) this.findViewById(R.id.Txt_Nombre_Signo);
+        Txt_Fechas = (TextView) this.findViewById(R.id.Txt_Fechas);
+        ImgVCentral = (ImageView) this.findViewById(R.id.ImgVCentral);
         cambiarCentro(1);
     }
 
@@ -70,5 +73,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void cambiarCentro(int i) {
+        id = i;
+        switch (i){
+            case 1:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.aries));
+                Txt_Fechas.setText(getResources().getString(R.string.aries_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.aries));
+                break;
+
+            case 2:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.tauro));
+                Txt_Fechas.setText(getResources().getString(R.string.tauro_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.tauro));
+                break;
+
+            case 3:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.geminis));
+                Txt_Fechas.setText(getResources().getString(R.string.geminis_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.geminis));
+                break;
+
+            case 4:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.cancer));
+                Txt_Fechas.setText(getResources().getString(R.string.cancer_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.cancer));
+                break;
+
+            case 5:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.leo));
+                Txt_Fechas.setText(getResources().getString(R.string.leo_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.leo));
+                break;
+
+            case 6:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.virgo));
+                Txt_Fechas.setText(getResources().getString(R.string.virgo_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.virgo));
+                break;
+
+            case 7:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.libra));
+                Txt_Fechas.setText(getResources().getString(R.string.libra_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.libra));
+                break;
+
+            case 8:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.escorpio));
+                Txt_Fechas.setText(getResources().getString(R.string.escorpio_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.escorpio));
+                break;
+
+            case 9:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.sagitario));
+                Txt_Fechas.setText(getResources().getString(R.string.sagitario_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.sagitario));
+                break;
+
+            case 10:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.carpicornio));
+                Txt_Fechas.setText(getResources().getString(R.string.capricornio_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.capricornio));
+                break;
+
+            case 11:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.acuario));
+                Txt_Fechas.setText(getResources().getString(R.string.acuario_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.acuario));
+                break;
+
+            case 12:
+                Txt_Nombre_Signo.setText(getResources().getString(R.string.piscis));
+                Txt_Fechas.setText(getResources().getString(R.string.piscis_f));
+                ImgVCentral.setImageDrawable(getResources().getDrawable(R.drawable.piscis));
+                break;
+        }
+    }
+
+    public void toSigno(View view) {
+        Intent intent = new Intent(this, SignoActivity.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 }
--- app/app/src/main/java/com/sime/itanortegaortega/astross/SignoActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/SignoActivity.java
@@ -1,13 +1,104 @@
 package com.sime.itanortegaortega.astross;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignoActivity extends AppCompatActivity {
+
+    int id;
+    TextView Txt_Nombre_Signo_B = null;
+    TextView Txt_Fechas_B = null;
+    ImageView ImgBanner = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signo);
+        Intent intent = getIntent();
+        Txt_Nombre_Signo_B = (TextView) this.findViewById(R.id.Txt_Nombre_Signo_Banner);
+        Txt_Fechas_B = (TextView) this.findViewById(R.id.Txt_Fechas_Banner);
+        ImgBanner = (ImageView) this.findViewById(R.id.ImgBanner);
+        id = (int) intent.getIntExtra("id", 1);
+        cambiarBanner(id);
+    }
+
+    private void cambiarBanner(int i) {
+        id = i;
+        switch (i){
+            case 1:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.aries));
+                Txt_Fechas_B.setText(getResources().getString(R.string.aries_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.aries));
+                break;
+
+            case 2:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.tauro));
+                Txt_Fechas_B.setText(getResources().getString(R.string.tauro_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.tauro));
+                break;
+
+            case 3:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.geminis));
+                Txt_Fechas_B.setText(getResources().getString(R.string.geminis_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.geminis));
+                break;
+
+            case 4:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.cancer));
+                Txt_Fechas_B.setText(getResources().getString(R.string.cancer_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.cancer));
+                break;
+
+            case 5:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.leo));
+                Txt_Fechas_B.setText(getResources().getString(R.string.leo_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.leo));
+                break;
+
+            case 6:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.virgo));
+                Txt_Fechas_B.setText(getResources().getString(R.string.virgo_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.virgo));
+                break;
+
+            case 7:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.libra));
+                Txt_Fechas_B.setText(getResources().getString(R.string.libra_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.libra));
+                break;
+
+            case 8:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.escorpio));
+                Txt_Fechas_B.setText(getResources().getString(R.string.escorpio_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.escorpio));
+                break;
+
+            case 9:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.sagitario));
+                Txt_Fechas_B.setText(getResources().getString(R.string.sagitario_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.sagitario));
+                break;
+
+            case 10:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.carpicornio));
+                Txt_Fechas_B.setText(getResources().getString(R.string.capricornio_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.capricornio));
+                break;
+
+            case 11:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.acuario));
+                Txt_Fechas_B.setText(getResources().getString(R.string.acuario_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.acuario));
+                break;
+
+            case 12:
+                Txt_Nombre_Signo_B.setText(getResources().getString(R.string.piscis));
+                Txt_Fechas_B.setText(getResources().getString(R.string.piscis_f));
+                ImgBanner.setImageDrawable(getResources().getDrawable(R.drawable.piscis));
+                break;
+        }
     }
 }
