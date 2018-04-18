93c58ab8499b41ec217146c08bdf1f299918173e
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-04-17 21:49:48 -0500

    Agregado evento click

--- app/app/src/main/java/com/sime/itanortegaortega/astross/MainActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/MainActivity.java
@@ -2,12 +2,73 @@ package com.sime.itanortegaortega.astross;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView signo = null;
+    TextView fechas = null;
+    ImageView img_view = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signo = (TextView) this.findViewById(R.id.Txt_Nombre_Signo);
+        fechas = (TextView) this.findViewById(R.id.Txt_Fechas);
+        img_view = (ImageView) this.findViewById(R.id.ImgVCentral);
+        cambiarCentro(1);
+    }
+
+    public void setAries(View view) {
+        cambiarCentro(1);
+    }
+
+    public void setTauro(View view) {
+        cambiarCentro(2);
+    }
+
+    public void setGeminis(View view) {
+        cambiarCentro(3);
+    }
+
+    public void setCancer(View view) {
+        cambiarCentro(4);
+    }
+
+    public void setLeo(View view) {
+        cambiarCentro(5);
+    }
+
+    public void setVirgo(View view) {
+        cambiarCentro(6);
+    }
+
+    public void setLibra(View view) {
+        cambiarCentro(7);
+    }
+
+    public void setEscorpio(View view) {
+        cambiarCentro(8);
+    }
+
+    public void setSagitario(View view) {
+        cambiarCentro(9);
+    }
+
+    public void setCapricornio(View view) {
+        cambiarCentro(10);
+    }
+
+    public void setAcuario(View view) {
+        cambiarCentro(11);
+    }
+
+    public void setPiscis(View view) {
+        cambiarCentro(12);
+    }
+
+    private void cambiarCentro(int i) {
     }
 }
