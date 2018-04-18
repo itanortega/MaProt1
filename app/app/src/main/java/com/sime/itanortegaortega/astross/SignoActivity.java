efbf83f7004559972d1eefa590fe02e8509e3290
Christian Ortega <csortega@iucesmag.edu.co>
2018-04-18 09:34:45 -0500

    Agregado menu

--- app/app/src/main/java/com/sime/itanortegaortega/astross/SignoActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/SignoActivity.java
@@ -3,6 +3,9 @@ package com.sime.itanortegaortega.astross;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +15,7 @@ public class SignoActivity extends AppCompatActivity {
     TextView Txt_Nombre_Signo_B = null;
     TextView Txt_Fechas_B = null;
     ImageView ImgBanner = null;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,26 @@ public class SignoActivity extends AppCompatActivity {
         ImgBanner = (ImageView) this.findViewById(R.id.ImgBanner);
         id = (int) intent.getIntExtra("id", 1);
         cambiarBanner(id);
+        toolbar = (Toolbar) findViewById(R.id.id_tb_toolbar);
+
+        showToolbar("titulo", true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
 
     private void cambiarBanner(int i) {
