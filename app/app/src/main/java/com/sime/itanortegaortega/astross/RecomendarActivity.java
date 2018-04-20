0447803e604a8c5a9f527a05853ee4e6e83756e5
Christian Ortega <csortega@iucesmag.edu.co>
2018-04-20 13:55:02 -0500

    Recomendar

--- app/app/src/main/java/com/sime/itanortegaortega/astross/RecomendarActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/RecomendarActivity.java
@@ -5,8 +5,13 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class RecomendarActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class RecomendarActivity extends AppCompatActivity {
     TextView Txt_Nombre_Signo_B = null;
     TextView Txt_Fechas_B = null;
     ImageView ImgBanner = null;
+    TextView Txt_Email = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,7 @@ public class RecomendarActivity extends AppCompatActivity {
         Txt_Nombre_Signo_B = (TextView) this.findViewById(R.id.Txt_Nombre_Signo_Banner);
         Txt_Fechas_B = (TextView) this.findViewById(R.id.Txt_Fechas_Banner);
         ImgBanner = (ImageView) this.findViewById(R.id.ImgBanner);
+        Txt_Email = (TextView) this.findViewById(R.id.Txt_Email);
 
         Txt_Nombre_Signo_B.setText(Utilidades.get_nombre_signo(this, id));
         Txt_Fechas_B.setText(Utilidades.get_fecha_signo(this, id));
@@ -41,5 +48,24 @@ public class RecomendarActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    public void recomendarApp(View view) {
+        String text = getResources().getString(R.string.enviado) + " " + Txt_Email.getText();
+        int duration = Toast.LENGTH_SHORT;
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        if(Txt_Email.getText().toString().trim().isEmpty()){
+            Toast toast = Toast.makeText(this, getResources().getString(R.string.vacio), duration);
+            toast.show();
+        }if(!pattern.matcher(Txt_Email.getText()).matches()){
+            Toast toast = Toast.makeText(this, getResources().getString(R.string.correonovalido), duration);
+            toast.show();
+        }else{
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+            Txt_Email.setText("");
+        }
+
     }
 }
